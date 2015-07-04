f04ed49ce10363dbe79ff03d635d8cc164852292
Meike Mertsch <nmmertsch@gmail.com>
2015-07-04 12:24:43 +0200

    added autoexpectations

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [expectations "2.1.2"]])
+                 [expectations "2.1.2"]]
+  :plugins [[lein-autoexpect "1.6.0"]])
