1215de47d2d42db5c3277a9f91d8d1e85957e203
Meike Mertsch <nmmertsch@gmail.com>
2015-07-04 21:36:17 +0200

    removed clutter

--- test/cardboard/core_test.clj
+++ test/cardboard/core_test.clj
@@ -25,8 +25,6 @@
 (expect char-b-instructions (instructions-for "b"))
 (expect (concat char-a-instructions '(((1 17))) char-b-instructions) (instructions-for "ab"))
 
-(concat char-a-instructions '(((1 17))) char-b-instructions)
-
 ;----- Writing The Instructions To A File
 (def char-a-string "1-5 6-7 8-10 11 12-17\n1-4 5-8 9-10 11-12 13-17\n1-4 5-6 7 8-9 10 11-12 13-17\n1-4 5-6 7-8 9 10 11-12 13-17\n1-5 6-12 13-17\n1-4 5-11 12-17\n1-4 5 6-17")
 (def char-b-string "1-4 5-15 16-17\n1-4 5-15 16-17\n1-5 6 7-9 10-11 12-17\n1-4 5-6 7-10 11-12 13-17\n1-4 5-7 8-9 10-12 13-17\n1-5 6-11 12-17\n1-6 7-10 11-17")
