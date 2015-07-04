6e07c15c944d823c1f379ba3b60f8e878c5e1d72
Meike Mertsch <nmmertsch@gmail.com>
2015-07-04 16:09:18 +0200

    added/changed letter b/B

--- test/cardboard/core_test.clj
+++ test/cardboard/core_test.clj
@@ -13,20 +13,21 @@
     ((1 6) (7) (8 10) (11 12) (13 17))))
 
 (def char-b-instructions
-  '(((1 3) (4 6) (7) (8 11) (12 14))
-   ((1 2) (3 4) (5 6) (7 8) (9 10) (11 12) (13 14))
-   ((1 2) (3 4) (5 6) (7 8) (9 10) (11 12) (13 14))
-   ((1 2) (3 12) (13 14))))
+  '(((1 7) (8 11) (12 17))
+    ((1 6) (7 12) (13 17))
+    ((1 5) (6 8) (9 10) (11 13) (14 17))
+    ((1 5) (6 7) (8 11) (12 13) (14 17))
+    ((1 6) (7 8) (9 11) (12) (13 17))
+    ((1 2) (3 13) (14 17))
+    ((1 2) (3 13) (14 17))))
 
 (expect char-a-instructions (instructions-for "a"))
 (expect char-b-instructions (instructions-for "b"))
 (expect (concat char-a-instructions '(((1 14))) char-b-instructions) (instructions-for "ab"))
-(concat char-a-instructions '((1 14)) char-b-instructions)
-
 
 ;----- Writing The Instructions To A File
 (def char-a-string "1-12 13 14-17\n1-6 7-13 14-17\n1-5 6-12 13-17\n1-5 6-7 8 9 10-11 12-13 14-17\n1-5 6-7 8 9-10 11 12-13 14-17\n1-5 6-7 8-9 10-13 14-17\n1-6 7 8-10 11-12 13-17")
-(def char-b-string "1-3 4-6 7 8-11 12-14\n1-2 3-4 5-6 7-8 9-10 11-12 13-14\n1-2 3-4 5-6 7-8 9-10 11-12 13-14\n1-2 3-12 13-14")
+(def char-b-string "1-7 8-11 12-17\n1-6 7-12 13-17\n1-5 6-8 9-10 11-13 14-17\n1-5 6-7 8-11 12-13 14-17\n1-6 7-8 9-11 12 13-17\n1-2 3-13 14-17\n1-2 3-13 14-17")
 
 (expect-let [placeholder (save-instructions-for "a")] char-a-string (slurp "instructions.txt"))
 (expect-let [placeholder (save-instructions-for "b")] char-b-string (slurp "instructions.txt"))
