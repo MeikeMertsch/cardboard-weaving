d3c74046c4ede9c2e5cb6a6d1dc87478644d355c
Meike Mertsch <nmmertsch@gmail.com>
2015-07-08 20:32:24 +0200

    changed the tests to new preconditions

--- test/cardboard/font_interpreter_test.clj
+++ test/cardboard/font_interpreter_test.clj
@@ -4,10 +4,10 @@
             [cardboard.common-test-data :refer :all]))
 
 ;----- Row Partitions
-(expect [4] (pack-sizes "0000"))
-(expect [1 2 1] (pack-sizes "0110"))
-(expect [1 1 2] (pack-sizes "0100"))
-(expect [3 9 2] (pack-sizes "00011111111100"))
+(expect [4] (pack-sizes (repeat 4 "0")))
+(expect [1 2 1] (pack-sizes (concat '("0") (repeat 2 "1") '("0"))))
+(expect [1 1 2] (pack-sizes (concat '("0") '("1") (repeat 2 "0"))))
+(expect [3 9 2] (pack-sizes (concat (repeat 3 "0") (repeat 9 "1") (repeat 2 "0"))))
 
 ;----- Partitions To Card Pack Rows
 (expect [[1 4]] (card-pack-row-of [4]))
@@ -15,9 +15,9 @@
 (expect [[1 3] [4 12] [13 14]] (card-pack-row-of [3 9 2]))
 
 ;----- Interprete Pattern Row
-(expect [[1 4]] (row-of-pattern "0000"))
-(expect [[1] [2 3] [4]] (row-of-pattern "0110"))
-(expect [[1 3] [4 12] [13 14]] (row-of-pattern "00011111111100"))
+(expect [[1 4]] (row-of-pattern (repeat 4 "0")))
+(expect [[1] [2 3] [4]] (row-of-pattern (concat '("0") (repeat 2 "1") '("0"))))
+(expect [[1 3] [4 12] [13 14]] (row-of-pattern (concat (repeat 3 "0") (repeat 9 "1") (repeat 2 "0"))))
 
 ;----- Instructions For Letters
 (expect char-a-instructions (instructions-for "a"))
