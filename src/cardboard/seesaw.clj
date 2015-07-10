31aa2efdbf374d6477b5f9ef314aeb50473db197
Meike Mertsch <nmmertsch@gmail.com>
2015-07-10 22:08:04 +0200

    Playing around with Seesaw

--- src/cardboard/seesaw.clj
+++ src/cardboard/seesaw.clj
@@ -1,15 +1,21 @@
 (ns cardboard.seesaw
   (:gen-class)
-  (:require [seesaw.core :as seesaw]))
+  (:require [seesaw.core :as seesaw])
+  (:require [seesaw.font :as font])
+  (:require [cardboard.font :refer :all]))
+
+(seesaw/native!)
+
+(def hagrid-string (apply str (interpose "\n" (concat (map #(apply str %) (pattern-of "Hagrid"))))))
+
+(def mytext (seesaw/text :id :hagrid :text hagrid-string :multi-line? true :font (font/font "Courier")))
 
 (def window
   (seesaw/frame
-    :title "First Example"
-    :content "hello world"
-    :width 200
-    :height 50))
+    :title "Cardboard Weaving Patterns"
+    :content mytext
+    :width 400
+    :height 600))
 
 (seesaw/show! window)
-
-
 
