c66892693ba1374fcd95fb7a59dcd5d065e91579
Meike Mertsch <nmmertsch@gmail.com>
2015-07-11 22:41:47 +0200

    got rid of tests that are no longer valuable

--- test/cardboard/core_test.clj
+++ test/cardboard/core_test.clj
@@ -4,14 +4,9 @@
             [cardboard.layouter :refer :all]
             [cardboard.common-test-data :refer :all]))
 
-(def default-file "instructions.txt")
-(def some-other-test-file "instructions-new.txt")
-
-;----- Writing The Instructions To A File
-(expect-let [placeholder (save-instructions-for "a")] (finish-layout "a" char-a-instructions) (slurp default-file))
-(expect-let [placeholder (save-instructions-for "b")] (finish-layout "b" char-b-instructions) (slurp default-file))
-(expect-let [placeholder (save-instructions-for "ab")] (finish-layout "ab" (concat char-a-instructions '(((1 17))) char-b-instructions)) (slurp default-file))
+(def core-test-file "instructions-new.txt")
 
 ;----- Writing The Instructions To A Specified File
-(expect-let [placeholder (save-instructions-for "a" some-other-test-file)] (finish-layout "a" char-a-instructions) (slurp some-other-test-file))
-(expect-let [placeholder (save-instructions-for "ab" some-other-test-file)] (finish-layout "ab" (concat char-a-instructions '(((1 17))) char-b-instructions)) (slurp some-other-test-file))
+(expect-let [placeholder (save-instructions-for "a" core-test-file)] (finish-layout "a" char-a-instructions) (slurp core-test-file))
+(expect-let [placeholder (save-instructions-for "b" core-test-file)] (finish-layout "b" char-b-instructions) (slurp core-test-file))
+(expect-let [placeholder (save-instructions-for "ab" core-test-file)] (finish-layout "ab" (concat char-a-instructions '(((1 17))) char-b-instructions)) (slurp core-test-file))
