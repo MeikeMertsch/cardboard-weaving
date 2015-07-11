ccc59940525704b78715aaaccc58aa51ed93e32d
Meike Mertsch <nmmertsch@gmail.com>
2015-07-11 22:43:54 +0200

    codestyle

--- src/cardboard/core.clj
+++ src/cardboard/core.clj
@@ -3,8 +3,8 @@
   (:require [cardboard.font_interpreter :refer :all])
   (:require [cardboard.layouter :refer :all]))
 
-(defn save-instructions-for
-  ([string file] (->> (instructions-for string)
-                      (finish-layout string)
-                      (save file))))
+(defn save-instructions-for [string file]
+  (->> (instructions-for string)
+       (finish-layout string)
+       (save file)))
 
