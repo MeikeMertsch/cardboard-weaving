aa80833be39c6bffeaa3aad9e302cb8da86a4d86
Meike Mertsch <nmmertsch@gmail.com>
2015-07-11 22:24:58 +0200

    made test a bit clearer

--- test/cardboard/core_test.clj
+++ test/cardboard/core_test.clj
@@ -4,11 +4,14 @@
             [cardboard.layouter :refer :all]
             [cardboard.common-test-data :refer :all]))
 
+(def default-file "instructions.txt")
+(def some-other-test-file "instructions-new.txt")
+
 ;----- Writing The Instructions To A File
-(expect-let [placeholder (save-instructions-for "a")] (finish-layout "a" char-a-instructions) (slurp "instructions.txt"))
-(expect-let [placeholder (save-instructions-for "b")] (finish-layout "b" char-b-instructions) (slurp "instructions.txt"))
-(expect-let [placeholder (save-instructions-for "ab")] (finish-layout "ab" (concat char-a-instructions '(((1 17))) char-b-instructions)) (slurp "instructions.txt"))
+(expect-let [placeholder (save-instructions-for "a")] (finish-layout "a" char-a-instructions) (slurp default-file))
+(expect-let [placeholder (save-instructions-for "b")] (finish-layout "b" char-b-instructions) (slurp default-file))
+(expect-let [placeholder (save-instructions-for "ab")] (finish-layout "ab" (concat char-a-instructions '(((1 17))) char-b-instructions)) (slurp default-file))
 
 ;----- Writing The Instructions To A Specified File
-(expect-let [placeholder (save-instructions-for "a" "instructions-new.txt")] (finish-layout "a" char-a-instructions) (slurp "instructions-new.txt"))
-(expect-let [placeholder (save-instructions-for "ab" "instructions-new.txt")] (finish-layout "ab" (concat char-a-instructions '(((1 17))) char-b-instructions)) (slurp "instructions-new.txt"))
+(expect-let [placeholder (save-instructions-for "a" some-other-test-file)] (finish-layout "a" char-a-instructions) (slurp some-other-test-file))
+(expect-let [placeholder (save-instructions-for "ab" some-other-test-file)] (finish-layout "ab" (concat char-a-instructions '(((1 17))) char-b-instructions)) (slurp some-other-test-file))
