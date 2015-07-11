9787b003d8c85ee5df773b0972f23343df990907
Meike Mertsch <nmmertsch@gmail.com>
2015-07-11 22:36:39 +0200

    changed todo

--- src/cardboard/core.clj
+++ src/cardboard/core.clj
@@ -4,7 +4,7 @@
   (:require [cardboard.layouter :refer :all]))
 
 (defn save-instructions-for
-  ([string] (save-instructions-for string "instructions.txt")) ;TODO: put this string somewhere safe
+  ([string] (save-instructions-for string "instructions.txt")) ;TODO: Get rid of this logic again as not needed anymore.
   ([string file] (->> (instructions-for string)
                       (finish-layout string)
                       (save file))))
