c10dce6251bcda8bb2bc2b06ae6ea8b749f32a50
Meike Mertsch <nmmertsch@gmail.com>
2015-07-13 19:54:14 +0200

    added TODO

--- src/cardboard/print_layouter.clj
+++ src/cardboard/print_layouter.clj
@@ -4,7 +4,7 @@
 (defn pack-description [start-end-coll]
   (apply str (interpose "-" start-end-coll)))
 
-(defn convert-instructions [instructions]
+(defn convert-instructions [instructions]                    ;TODO: Look through with James
   (->> (map #(map pack-description %) instructions)
        (map #(clojure.string/join " " %))
        (interpose "\n")
