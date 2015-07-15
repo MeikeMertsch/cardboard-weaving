1b4731ea34c42601ba2dbaa90e5daf237b072239
Meike Mertsch <nmmertsch@gmail.com>
2015-07-15 08:59:51 +0200

    added constants and referred to it

--- src/cardboard/constants.clj
+++ src/cardboard/constants.clj
@@ -2,3 +2,7 @@
 
 (def empty-string "")
 (def empty-pattern '(()))
+(def new-line "\n")
+(def dash "-")
+(def space " ")
+(def header "Pattern for")
--- src/cardboard/print_layouter.clj
+++ src/cardboard/print_layouter.clj
@@ -1,24 +1,23 @@
 (ns cardboard.print_layouter
-  (:require [cardboard.instructions :as i]))
+  (:require [cardboard.instructions :as i])
+  (:require [cardboard.constants :refer :all]))
 
 (defn pack-description [start-end-coll]
-  (apply str (interpose "-" start-end-coll)))
+  (apply str (interpose dash start-end-coll)))
 
 (defn convert-instructions [instructions]                    ;TODO: Look through with James
   (->> (map #(map pack-description %) instructions)
-       (map #(clojure.string/join " " %))
-       (interpose "\n")
+       (map #(clojure.string/join space %))
+       (interpose new-line)
        clojure.string/join))
 
 (defn compose-layout [string instructions]
-  (let [header "Pattern for" ;TODO: Put this string somewhere safe
-        new-line "\n"]
-    (apply str header
-           new-line
-           string
-           new-line
-           new-line
-           (convert-instructions instructions))))
+  (apply str header
+             new-line
+             string
+             new-line
+             new-line
+             (convert-instructions instructions)))
 
 (defn string-n-pattern->layout [string pattern]
   (->> (i/pattern->instructions pattern)
