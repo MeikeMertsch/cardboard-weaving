ea6182dacda898e8efdf96878a079e14470e245b
Meike Mertsch <nmmertsch@gmail.com>
2015-07-15 08:33:55 +0200

    changed reference style

--- src/cardboard/core.clj
+++ src/cardboard/core.clj
@@ -1,15 +1,15 @@
 (ns cardboard.core
-  (:require [cardboard.saver :refer :all])
-  (:require [cardboard.pattern :refer :all])
-  (:require [cardboard.print_layouter :refer :all]))
+  (:require [cardboard.pattern :as p])
+  (:require [cardboard.print_layouter :as l])
+  (:require [cardboard.saver :as s]))
 
 (defn save-instructions-for [string file-path]
-  (->> (string->pattern string)
-       (string-n-pattern->layout string)
-       (save file-path)))
+  (->> (p/string->pattern string)
+       (l/string-n-pattern->layout string)
+       (s/save file-path)))
 
 (defn pattern-in-rows [string]
   (if (= "" string)
     '(())
-    (->> (string->pattern string)
+    (->> (p/string->pattern string)
            (apply map concat))))
