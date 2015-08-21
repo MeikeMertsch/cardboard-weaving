c1ba9c60dd0f4704ccc13264cb9e949c87e0285e
Meike Mertsch <nmmertsch@gmail.com>
2015-08-21 13:59:54 +0200

    changed tests according to gitignore

--- test/cardboard/input_test.clj
+++ test/cardboard/input_test.clj
@@ -23,11 +23,11 @@
 ;----- Saves With Proper Extension
 (def the-string "abc")
 
-(expect [[the-string "Hagrid II.txt"]]
+(expect [[the-string "test_Hagrid II.txt"]]
         (side-effects [cc/save-instructions-for]
-                      (save-instructions the-string false "Hagrid II")))
+                      (save-instructions the-string false "test_Hagrid II")))
 
-(expect [[the-string "Meike II.txt"]]
+(expect [[the-string "test_Meike II.txt"]]
         (side-effects [cc/save-instructions-for]
-                      (save-instructions the-string false "Meike II.txt")))
+                      (save-instructions the-string false "test_Meike II.txt")))
 
