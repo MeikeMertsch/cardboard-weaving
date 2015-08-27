7462813bfcb4c6581f675fa4de5c8c7b7201f3e9
Meike Mertsch <nmmertsch@gmail.com>
2015-08-27 17:38:28 +0200

    renaming

--- src/cardboard/preview_canvas.clj
+++ src/cardboard/preview_canvas.clj
@@ -8,37 +8,37 @@
 (def preview-canvas (canvas :paint nil))
 (def style-foreground (sg/style :background (scol/color :black)))
 (def style-background (sg/style :background nil))
-(def rect-height 2)
-(def rect-width 3)
+(def pixel-height 2)
+(def pixel-width 3)
 
-(defn rectangle [column row]
-  (sg/rect (* rect-width column) (* rect-height row) rect-width rect-height))
+(defn pixel [column row]
+  (sg/rect (* pixel-width column) (* pixel-height row) pixel-width pixel-height))
 
 ;;; Preview
-(defn pixel-state [pixel]
-  (if (= pixel foreground-pixel)
+(defn pixel-style [filling]
+  (if (= filling foreground-pixel)
     style-foreground
     style-background))
 
 (defn row-pixels [row row-number]
   (for [column-number (range (count row))
-        :let [pixel (nth row column-number)
-              rect (rectangle column-number row-number)
-              style (pixel-state pixel)]]
-    [rect style]))
+        :let [pxl (pixel column-number row-number)
+              filling (nth row column-number)
+              style (pixel-style filling)]]
+    [pxl style]))
 
-(defn rectangles [pattern]
+(defn pixels [pattern]
   (->> (for [row-number (range (count pattern))
              :let [row (nth pattern row-number)]]
          (row-pixels row row-number))
        (apply concat)))
 
-(defn paint [rects _ graphic]
-  (doseq [[rect style] rects]
-    (sg/draw graphic rect style)))
+(defn paint [pxls _ graphic]
+  (doseq [[pxl style] pxls]
+    (sg/draw graphic pxl style)))
 
 (defn preview [pattern-in-rows]
-  (config! preview-canvas :paint #(paint (rectangles pattern-in-rows) %1 %2)))
+  (config! preview-canvas :paint #(paint (pixels pattern-in-rows) %1 %2)))
 
 
 
