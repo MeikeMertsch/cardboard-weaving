60c39d3aa289299ad8f9da30e3dc1c8b2edcd374
Meike Mertsch <nmmertsch@gmail.com>
2015-08-29 13:52:21 +0200

    fiddled with mouse-clicks

--- src/cardboard/character_editing.clj
+++ src/cardboard/character_editing.clj
@@ -1,8 +1,10 @@
 (ns cardboard.character-editing
   (:require [seesaw.core :refer :all]
+            [seesaw.graphics :as sg]
+            [seesaw.color :as scol]
+            [seesaw.mouse :as mou]
             [cardboard.preview-canvas :as pre]
             [cardboard.pattern :as pat]
-            [cardboard.default_chars :as dc]
             [cardboard.constants :refer :all]))
 
 (def editing-window
@@ -11,8 +13,16 @@
     :width (* 10 (zoom-size :width))
     :height (+ status-bar-height (* 17 (zoom-size :height)))))
 
+(defn new-pxl [loc]
+  [[(sg/rect (first loc) (last loc) 15 10) (sg/style :background (scol/color :red))]])
+
+(defn do-something [character-canvas caller]
+  (config! character-canvas :paint #(pre/paint (new-pxl (mou/location caller))
+                                               %1 %2)))
+
 (defn paint-canvas [character character-canvas]
   (pre/preview character-canvas zoom-size (pat/string->pattern character))
+  (listen character-canvas :mouse-clicked (partial do-something character-canvas))
   character-canvas)
 
 (defn open [character]
