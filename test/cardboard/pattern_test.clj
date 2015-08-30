cb3b092590d71e7357acf92b6d961d1b4070ee92
Meike Mertsch <nmmertsch@gmail.com>
2015-08-30 13:46:20 +0200

    changed order

--- test/cardboard/pattern_test.clj
+++ test/cardboard/pattern_test.clj
@@ -9,14 +9,14 @@
 (expect '(" ") (str->chars " "))
 (expect '(" " "5") (str->chars " 5"))
 
+;;; Turn The Pattern Into A Matrix
+(expect (repeat 17 '("0" "0" "0")) (char-patterns->matrix space))
+(expect (repeat 17 '("0")) (char-patterns->matrix letter-space))
+
 ;;; Pattern Of A String
 (expect (repeat 17 (repeat 3 "0")) (string->pattern " "))
 (expect (repeat 17 (repeat 7 "0")) (string->pattern "  "))
 (expect (repeat 17 (repeat 3 "0")) (string->pattern " 5"))
-
-;;; Turn The Pattern Into A Matrix
-(expect (repeat 17 '("0" "0" "0")) (char-patterns->matrix space))
-(expect (repeat 17 '("0")) (char-patterns->matrix letter-space))
 
 ;;; String Of A Pattern
 (expect "12\n34\n56" (pattern->string '(("1" "2") ("3" "4") ("5" "6"))))
