1429ae469078decc3c336f99dfdb6320323d0d41
Meike Mertsch <nmmertsch@gmail.com>
2015-08-30 16:25:44 +0200

    added todo

--- src/cardboard/pixel_size.clj
+++ src/cardboard/pixel_size.clj
@@ -13,3 +13,4 @@
     [(* (pixel-size :width) (max (first size-in-pixels) minimum-width))
      :by
      (+ status-bar-height button-bar-height (* (pixel-size :height) (last size-in-pixels)))]))
+;; TODO: Better naming. With the addition to the height, it's bound to the character windows
