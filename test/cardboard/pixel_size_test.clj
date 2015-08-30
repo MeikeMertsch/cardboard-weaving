36c2975efa84e20ef1e8a65e3f1f49fe93f08bbc
Meike Mertsch <nmmertsch@gmail.com>
2015-08-30 16:22:49 +0200

    added tests for sizing

--- test/cardboard/pixel_size_test.clj
+++ test/cardboard/pixel_size_test.clj
@@ -1,6 +1,18 @@
 (ns cardboard.pixel-size-test
   (require [expectations :refer :all]
-           [cardboard.pixel-size :refer :all]))
+           [cardboard.pixel-size :refer :all]
+           [cardboard.constants :refer :all]))
 
+;;; Sizes In Pixels
 (expect [3 17] (size " "))
 (expect [22 17] (size "abc"))
+
+;;; Absolute Sizes
+(expect [330 :by 390] (absolute-size zoom-size "   "))
+(expect [21 :by 84] (absolute-size preview-size "a"))
+
+;;; Minimum Width Is Six Pixels
+(expect [180 :by 390] (absolute-size zoom-size " "))
+
+;;; Minimum Height Is Status Bar + Button Panel
+(expect [0 :by (+ status-bar-height button-bar-height)] (absolute-size {:width 0 :height 0} "irrelephant"))
