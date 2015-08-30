681a78042e695232a89ebc41a5ac197f8ef5f34c
Meike Mertsch <nmmertsch@gmail.com>
2015-08-30 13:40:42 +0200

    fixed test

--- test/cardboard/core_test.clj
+++ test/cardboard/core_test.clj
@@ -7,11 +7,16 @@
 (def core-test-file "instructions-new.txt")
 (def placeholder)
 
-;----- Writing The Instructions To A Specified File
+;;; Writing The Instructions To A Specified File
 (expect-let [placeholder (save-instructions-for "a" core-test-file)] (#(string-n-pattern->layout % (p/string->pattern %)) "a") (slurp core-test-file))
 (expect-let [placeholder (save-instructions-for "b" core-test-file)] (#(string-n-pattern->layout % (p/string->pattern %)) "b") (slurp core-test-file))
 (expect-let [placeholder (save-instructions-for "ab" core-test-file)] (#(string-n-pattern->layout % (p/string->pattern %)) "ab") (slurp core-test-file))
 
-;----- Give Me The Pattern To A String
+;;; Give Me The Pattern To A String
 (expect (repeat 17 (repeat 7 "0")) (pattern-in-rows "  "))
 (expect '(()) (pattern-in-rows ""))
+
+;;; Figure Proper Filename For Saving Characters
+(expect 71 (character-file-name "G"))
+(expect 32 (character-file-name " "))
+(expect 117 (character-file-name "u"))
