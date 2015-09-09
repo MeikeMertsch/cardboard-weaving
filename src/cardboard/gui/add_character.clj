fb412e352519560f6dfcfb3c5519c26cd3499c3a
Meike Mertsch <nmmertsch@gmail.com>
2015-09-09 16:27:18 +0200

    add new character in correct font

--- src/cardboard/gui/add_character.clj
+++ src/cardboard/gui/add_character.clj
@@ -8,18 +8,22 @@
 
 (def input-for-character (text))
 (def input-for-width (text))
+(def this-font (atom default-font))
 (def width-panel (grid-panel :columns 2
                              :items ["Character"
                                      input-for-character
                                      "Width"
                                      input-for-width]))
 
+(defn- current-font []
+  (deref this-font))
+
 (defn- default-pattern []
   (repeat 17 (repeat (bigint (value input-for-width)) background-pixel)))
 
 (defn- ok-function [_]
   (let [character (first (value input-for-character))]
-    (sav/save-character character (default-pattern) "custom")
+    (sav/save-character character (default-pattern) (current-font))
     (ce/open "custom" character))
   (alert "yeay"))
 
@@ -32,7 +36,8 @@
     :width 240
     :height 150))
 
-(defn open-preferences [_]
+(defn open-preferences [font]
+  (reset! this-font font)
   (show! ac-window))
 
 
