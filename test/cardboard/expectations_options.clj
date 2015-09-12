c1eadaf56202e91f0171efb3fb2ec74df2086abf
Meike Mertsch <nmmertsch@gmail.com>
2015-09-12 10:56:58 +0200

    fixed bug of wrong font/listener config through test setup

--- test/cardboard/expectations_options.clj
+++ test/cardboard/expectations_options.clj
@@ -1,6 +1,11 @@
 (ns cardboard.expectations-options
-  (require [cardboard.font :as f]
+  (require [seesaw.core :refer :all]
+           [cardboard.font :as f]
+           [cardboard.gui.main-ui :as mu]
            [cardboard.constants :refer :all]))
 
 (defn start-with-test-data {:expectations-options :before-run} []
   (f/update-mapping! default-font))
+
+(defn end-with-proper-data {:expectations-options :after-run} []
+  (f/update-mapping! (value mu/font-choice)))
